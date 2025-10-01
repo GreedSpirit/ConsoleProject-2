8d9bba7f96374e232c9d581ddaf750818a3337d6
GreedSpirit <dlwjdgus0918@naver.com>
2025-10-01 15:48:21 +0900

    [Fix] Clear Delete
    계속 프레임이 끊기듯 출력되는 문제였던 Console.Clear을 삭제

--- Map.cs
+++ Map.cs
@@ -1,18 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Console_Project
 {
-	internal class Map
+	class Map
 	{
 		//인스턴스 생성 없이 바로 Map.DrawMap으로 맵을 만들 수 있도록 static 선언
 		static public void DrawMap()
 		{
-			Console.Clear();
-			for(int i = 0; i < GameManager.instance.worldX; i++)
+			for(int i = 0; i < GameManager.instance.worldX / 2; i++)
 			{
 				Console.Write("▩");
 			}
@@ -20,16 +15,21 @@ namespace Console_Project
 			for(int i = 0; i < GameManager.instance.worldY; i++)
 			{
 				Console.Write("▩");
-				for(int j = 0; j < GameManager.instance.worldX * 2 - 4; j++)
+				for(int j = 0; j < GameManager.instance.worldX - 4; j++)
 				{
 					Console.Write(" ");
 				}
 				Console.WriteLine("▩");
 			}
-			for (int i = 0; i < GameManager.instance.worldX; i++)
+			for (int i = 0; i < GameManager.instance.worldX / 2; i++)
 			{
 				Console.Write("▩");
 			}
+			Console.WriteLine();
+			
+			Console.WriteLine("Score : " + GameManager.instance.score + "      Bomb(B) : " + GameManager.instance.playerBomb
+				+ "      Health : " + GameManager.instance.playerHealth);
+			Console.WriteLine("Time : " + GameManager.stopWatch.ElapsedMilliseconds / 1000 + "." + GameManager.stopWatch.ElapsedMilliseconds % 1000); ;
 		}
 	}
 }
