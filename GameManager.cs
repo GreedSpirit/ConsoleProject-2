6c384f6c70064db329d816cf7d79f580938368d2
GreedSpirit <dlwjdgus0918@naver.com>
2025-10-01 15:46:36 +0900

    [Init] Game Clear & Over
    게임 클리어 및 오버의 판단과 출력 함수를 구현

--- GameManager.cs
+++ GameManager.cs
@@ -1,20 +1,62 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+
 
 namespace Console_Project
 {
+	public delegate void GameStartRoutine();
+	public delegate void ProjectileRoutine();
 	public class GameManager
 	{
-		public int worldX = 42;
+		public GameStartRoutine gameStartRoutine;
+		public ProjectileRoutine projectileRoutine;
+		public int worldX = 44; // 2로 나누었을 때 짝수가 되도록 설정하기
 		public int worldY = 25;
-		public float timer = 30;
+		public float maxTimer = 20;
 		public int difficulty;
+		public int playerHealth = 3;
+		public int score = 0;
+		public int playerBomb = 1;
+
+		static public Stopwatch stopWatch = new Stopwatch();
 
 		static public GameManager instance = new GameManager();
 
-		
+		public void ClearScreenByFilling()
+		{
+			Console.SetCursorPosition(0, 0);
+		}
+
+		public bool GameOver()
+		{
+			return playerHealth > 0;
+		}
+
+		public bool HardGameClear()
+		{
+			return stopWatch.ElapsedMilliseconds < maxTimer * 1000;
+		}
+
+		public bool GameClear()
+		{
+			return score <= 100;
+		}
+		public void DrawGameClear()
+		{
+			Console.Clear();
+			Console.SetWindowSize(200, 50);
+			Console.WriteLine("   ####     ##     #    #   ######  \r\n  ##  ##   #  #    ##  ##   #       \r\n ##       #    #   # ## #   #       \r\n ##  ###  ######   # ## #   ####    \r\n ##   ##  #    #   #    #   #       \r\n  ## ###  #    #   #    #   #       \r\n   ### #  #    #   #    #   ######  \r\n                                    \r\n");  
+			Console.WriteLine("   ####   #        ######     ##     #####      ##    \r\n  ##  ##  #        #         #  #    #    #     ##    \r\n ##       #        #        #    #   #    #     ##    \r\n ##       #        ####     ######   #####      ##    \r\n ##       #        #        #    #   #  #             \r\n  ##  ##  #        #        #    #   #   #      ##    \r\n   ####   ######   ######   #    #   #    #     ##    \r\n                                                      \r\n");
+		}
+
+		public void DrawGameOver()
+		{
+			Console.Clear();
+			Console.SetWindowSize(200, 50);
+			Console.WriteLine("   ####     ##     #    #   ######  \r\n  ##  ##   #  #    ##  ##   #       \r\n ##       #    #   # ## #   #       \r\n ##  ###  ######   # ## #   ####    \r\n ##   ##  #    #   #    #   #       \r\n  ## ###  #    #   #    #   #       \r\n   ### #  #    #   #    #   ######  \r\n                                    \r\n");
+			Console.WriteLine("   ###    #    #   ######   #####                              \r\n  ## ##   #    #   #        #    #                             \r\n ##   ##  #    #   #        #    #                             \r\n ##   ##   #  #    ####     #####                              \r\n ##   ##   #  #    #        #  #                               \r\n  ## ##     ##     #        #   #      ##       ##       ##    \r\n   ###      ##     ######   #    #     ##       ##       ##    \r\n                                                               \r\n");
+		}
+
+
 	}
 }
