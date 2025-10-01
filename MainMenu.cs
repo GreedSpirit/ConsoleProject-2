9ebd6f5d811bf39d8dae6e1bf4743cfd6204e4ee
GreedSpirit <dlwjdgus0918@naver.com>
2025-10-01 15:47:43 +0900

    [Fix] Enum Difficulty
    난이도를 단지 switch에서 숫자로 구분하던 것을 enum을 추가하여 수정

--- MainMenu.cs
+++ MainMenu.cs
@@ -1,16 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Console_Project
 {
-	internal class MainMenu
+	class MainMenu
 	{
+		enum difficulty
+		{
+			Title, Easy, Normal, Hard
+		}
 		//인스턴스 생성 없이 MainMenu.GameStart()로 바로 게임이 시작되도록 하기 위해서 static 선언, 이후 한 번의 게임이 끝나고 또 호출해야 함
 		static int menuCount = 5;
-		static public void GameStart()
+		static public void MenuStart()
 		{
 			DrawMenu();
 			DifficultyInput();
@@ -18,23 +18,20 @@ namespace Console_Project
 		static void DrawMenu()
 		{
 			for (int i = 0; i < menuCount; i++) {
-				Console.SetCursorPosition(GameManager.instance.worldX / 2, GameManager.instance.worldY / 4 + i);
-
-				switch (i)
+				switch ((difficulty)i)
 				{
-					case 0:
+					case difficulty.Title:
 						Console.WriteLine("메인 메뉴");
 						break;
-					case 1:
+					case difficulty.Easy:
 						Console.WriteLine("1. Easy");
 						break;
-					case 2:
+					case difficulty.Normal:
 						Console.WriteLine("2. Normal");
 						break;
-					case 3:
+					case difficulty.Hard:
 						Console.WriteLine("3. Hard");
 						break;
-
 
 					default:
 						Console.WriteLine($"{i}. 종료");
@@ -45,17 +42,18 @@ namespace Console_Project
 
 		static void DifficultyInput()
 		{
-			Console.SetCursorPosition(GameManager.instance.worldX / 2, GameManager.instance.worldY / 4 + menuCount);
+			
 			Console.Write("원하는 난이도 혹은 종료 숫자를 입력해주세요 : ");
 			int.TryParse(Console.ReadLine(), out GameManager.instance.difficulty);
 			while (!(GameManager.instance.difficulty > 0 && GameManager.instance.difficulty < menuCount))
 			{
-				Console.SetCursorPosition(GameManager.instance.worldX / 2, GameManager.instance.worldY / 4 + menuCount);
-				Console.WriteLine("                                                      ");
-				Console.SetCursorPosition(GameManager.instance.worldX / 2, GameManager.instance.worldY / 4 + menuCount);
+				Console.SetCursorPosition(0, menuCount);
+				Console.Write(new string(' ', Console.WindowWidth));
+				Console.SetCursorPosition(0, menuCount);
 				Console.Write($"1 ~ {menuCount - 1} 사이의 숫자만 입력해주세요 : ");
 				int.TryParse(Console.ReadLine(), out GameManager.instance.difficulty);
 			}
 		}
+
 	}
 }
