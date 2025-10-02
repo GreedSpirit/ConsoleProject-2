7493122ce36de590dc4d283e71200221b8717438
GreedSpirit <dlwjdgus0918@naver.com>
2025-10-02 10:58:59 +0900

    [Init] CustomList 구현
    제네릭 컬렉션에 있는 List가 아닌 자체 제작 자료구조를 구현했다.
    (Add, Count, 인덱스 접근 가능

---
+++ CustomList.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Console_Project
+{
+	public class CustomList<T>
+	{
+		private T[] _items;
+		private int _count;
+		private int _capacity;
+		public CustomList()
+		{
+			_capacity = 80;
+			_items = new T[_capacity];
+			_count = 0;
+		}
+		public int Count  // List.Count 구현
+		{
+			get { return _count; }
+		}
+		public void Add(T item) // List.Add 구현
+		{
+			if (_count == _capacity)
+			{
+				_capacity *= 2;
+				T[] newItems = new T[_capacity];
+
+				for (int i = 0; i < _count; i++)
+				{
+					newItems[i] = _items[i];
+				}
+
+				_items = newItems;
+			}
+
+			_items[_count] = item;
+			_count++;
+		}
+
+		public T this[int index] //List 인덱스 접근 구현
+		{
+			get
+			{
+				if (index < 0 || index >= _count)
+				{
+					throw new IndexOutOfRangeException();
+				}
+				return _items[index];
+			}
+			set
+			{
+				if (index < 0 || index >= _count)
+				{
+					throw new IndexOutOfRangeException();
+				}
+				_items[index] = value;
+			}
+		}
+	}
+}
