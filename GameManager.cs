311d429c437a83d5dd20ccfca6dd8bca06d11035
GreedSpirit <dlwjdgus0918@naver.com>
2025-10-02 12:22:52 +0900

    [fix] Screen size and timer
    밸런스 조정과 창 크기 조절

--- GameManager.cs
+++ GameManager.cs
@@ -10,9 +10,9 @@ namespace Console_Project
 	{
 		public GameStartRoutine gameStartRoutine;
 		public ProjectileRoutine projectileRoutine;
-		public int worldX = 44; // 2로 나누었을 때 짝수가 되도록 설정하기
+		public int worldX = 52; // 2로 나누었을 때 짝수가 되도록 설정하기
 		public int worldY = 25;
-		public float maxTimer = 20;
+		public float maxTimer = 40;
 		public int difficulty;
 		public int playerHealth = 3;
 		public int score = 0;
@@ -32,19 +32,19 @@ namespace Console_Project
 			return playerHealth > 0;
 		}
 
-		public bool HardGameClear()
+		public bool HardGameClear() // 시간을 버티면 되는 조건
 		{
 			return stopWatch.ElapsedMilliseconds < maxTimer * 1000;
 		}
 
-		public bool GameClear()
+		public bool GameClear() // 100개의 투사체가 사라지면 이기는 조건 (bomb는 제외)
 		{
-			return score <= 100;
+			return score <= 10;
 		}
 		public void DrawGameClear()
 		{
 			Console.Clear();
-			Console.SetWindowSize(200, 50);
+			Console.SetWindowSize(100, 30);
 			Console.WriteLine("   ####     ##     #    #   ######  \r\n  ##  ##   #  #    ##  ##   #       \r\n ##       #    #   # ## #   #       \r\n ##  ###  ######   # ## #   ####    \r\n ##   ##  #    #   #    #   #       \r\n  ## ###  #    #   #    #   #       \r\n   ### #  #    #   #    #   ######  \r\n                                    \r\n");  
 			Console.WriteLine("   ####   #        ######     ##     #####      ##    \r\n  ##  ##  #        #         #  #    #    #     ##    \r\n ##       #        #        #    #   #    #     ##    \r\n ##       #        ####     ######   #####      ##    \r\n ##       #        #        #    #   #  #             \r\n  ##  ##  #        #        #    #   #   #      ##    \r\n   ####   ######   ######   #    #   #    #     ##    \r\n                                                      \r\n");
 		}
@@ -52,7 +52,7 @@ namespace Console_Project
 		public void DrawGameOver()
 		{
 			Console.Clear();
-			Console.SetWindowSize(200, 50);
+			Console.SetWindowSize(100, 30);
 			Console.WriteLine("   ####     ##     #    #   ######  \r\n  ##  ##   #  #    ##  ##   #       \r\n ##       #    #   # ## #   #       \r\n ##  ###  ######   # ## #   ####    \r\n ##   ##  #    #   #    #   #       \r\n  ## ###  #    #   #    #   #       \r\n   ### #  #    #   #    #   ######  \r\n                                    \r\n");
 			Console.WriteLine("   ###    #    #   ######   #####                              \r\n  ## ##   #    #   #        #    #                             \r\n ##   ##  #    #   #        #    #                             \r\n ##   ##   #  #    ####     #####                              \r\n ##   ##   #  #    #        #  #                               \r\n  ## ##     ##     #        #   #      ##       ##       ##    \r\n   ###      ##     ######   #    #     ##       ##       ##    \r\n                                                               \r\n");
 		}
