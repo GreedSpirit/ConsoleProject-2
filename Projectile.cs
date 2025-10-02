202f5914970da520bfe9ede59931d3debca7719f
GreedSpirit <dlwjdgus0918@naver.com>
2025-10-02 12:27:17 +0900

    Update Projectile.cs
    x축 이동을 2칸씩 해야 글자 크기에 알맞기에 dx를 수정하고 콘솔 전체를 지우는 것이 아닌 이전 위치만 지우기 위해서 ex,ey를 추가.

--- Projectile.cs
+++ Projectile.cs
@@ -6,13 +6,15 @@ namespace Console_Project
 {
 	internal class Projectile
 	{
-		int[] dx = { 0, 0, 1, -1 }; //dir을 이용해서 이동시키기 위한 배열
+		int[] dx = { 0, 0, 2, -2 }; //dir을 이용해서 이동시키기 위한 배열
 		int[] dy = { 1, -1, 0, 0 };
+		private int _ex;
+		private int _ey;
 
-		int _posX; // dir이 1, 2 일 때
-		int _posY; // dir이 0, 1 일 때
-		int _dir;
-		bool _isLive;
+		private int _posX; // dir이 1, 2 일 때
+		private int _posY; // dir이 0, 1 일 때
+		private int _dir;
+		private bool _isLive;
 
 		public bool IsLive {  get { return _isLive; } set { _isLive = value; } }
 
@@ -24,17 +26,24 @@ namespace Console_Project
 		public void Move()
 		{
 			if (_isLive) {
+				_ex = _posX;
+				_ey = _posY;
+
 				_posX += dx[_dir];
 				_posY += dy[_dir];
 
 				if(_posY < 1 || _posY > GameManager.instance.worldY)
 				{
 					_isLive = false;
+					Console.SetCursorPosition(_ex, _ey);
+					Console.Write("  ");
 					GameManager.instance.score++;
 				}
-				if (_posX < 2 || _posX > GameManager.instance.worldX - 2)
+				if (_posX < 2 || _posX > GameManager.instance.worldX - 4)
 				{
 					_isLive = false;
+					Console.SetCursorPosition(_ex, _ey);
+					Console.Write("  ");
 					GameManager.instance.score++;
 				}
 			}
@@ -60,10 +69,10 @@ namespace Console_Project
 			_isLive = true;
 			Random rnd = new Random();
 
-			_posX = rnd.Next(2, GameManager.instance.worldX - 3);
+			_posX = rnd.Next(2, GameManager.instance.worldX - 4);
 			while (_posX % 2 != 0)
 			{
-				_posX = rnd.Next(2, GameManager.instance.worldX - 3);
+				_posX = rnd.Next(2, GameManager.instance.worldX - 4);
 			}
 			_dir = rnd.Next(0, 2);
 
@@ -82,29 +91,51 @@ namespace Console_Project
 			_isLive = true;
 			Random rnd = new Random();
 
-			_dir = rnd.Next(0, (int)(GameManager.stopWatch.ElapsedMilliseconds / (int)(GameManager.instance.maxTimer / 5 * 1000)));
+			_dir = rnd.Next(0, (int)(GameManager.stopWatch.ElapsedMilliseconds / ((int)(GameManager.instance.maxTimer / 5) * 1000)));
+			if (_dir > 3) _dir = 3; // 혹시 모르는 방어코드
 
-			_posX = rnd.Next(2, GameManager.instance.worldX - 3);
-			while (_posX % 2 != 0)
+			if (_dir == 0 || _dir == 1)
 			{
-				_posX = rnd.Next(2, GameManager.instance.worldX - 3);
-			}
-			_dir = rnd.Next(0, 2);
+				_posX = rnd.Next(2, GameManager.instance.worldX - 4);
+				while (_posX % 2 != 0)
+				{
+					_posX = rnd.Next(2, GameManager.instance.worldX - 4);
+				}
 
-			if (_dir == 0)
-			{
-				_posY = 0;
+				if (_dir == 0)
+				{
+					_posY = 0;
+				}
+				else
+				{
+					_posY = GameManager.instance.worldY + 1;
+				}
 			}
 			else
 			{
-				_posY = GameManager.instance.worldY + 1;
+				if (_dir == 2)
+				{
+					_posX = 0;
+				}
+				else
+				{
+					_posX = GameManager.instance.worldX - 2;
+				}
+
+				_posY = rnd.Next(1, GameManager.instance.worldY + 1);
 			}
+
 		}
 
 		public void Draw()
 		{
 			if (_isLive)
 			{
+				if(!(_ey < 1 || _ey > GameManager.instance.worldY || _ex < 2 || _ex > GameManager.instance.worldX - 4))
+				{
+					Console.SetCursorPosition(_ex, _ey);
+					Console.Write("  ");
+				}
 				Console.SetCursorPosition(_posX, _posY);
 				if (_dir == 0)
 				{
@@ -116,6 +147,18 @@ namespace Console_Project
 				{
 					Console.ForegroundColor = ConsoleColor.Blue;
 					Console.Write("△");
+					Console.ResetColor();
+				}
+				else if (_dir == 2)
+				{
+					Console.ForegroundColor = ConsoleColor.Cyan;
+					Console.Write("▷");
+					Console.ResetColor();
+				}
+				else if (_dir == 3)
+				{
+					Console.ForegroundColor = ConsoleColor.Gray;
+					Console.Write("◁");
 					Console.ResetColor();
 				}
 			}
@@ -139,7 +182,7 @@ namespace Console_Project
 						}
 						else if (GameManager.instance.difficulty == 3)
 						{
-							Program.projectiles[i].NormalCreateProjectile();
+							Program.projectiles[i].HardCreateProjectile();
 						}
 
 						return;
@@ -150,11 +193,11 @@ namespace Console_Project
 			Program.projectiles.Add(new Projectile());
 			if (GameManager.instance.difficulty == 1)
 			{
-				Program.projectiles.Last().EasyCreateProjectile();
+				Program.projectiles[Program.projectiles.Count - 1].EasyCreateProjectile();
 			}
 			else if (GameManager.instance.difficulty == 2)
 			{
-				Program.projectiles.Last().NormalCreateProjectile();
+				Program.projectiles[Program.projectiles.Count - 1].NormalCreateProjectile();
 			}
 
 		}
